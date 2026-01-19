ff5973b4e661bdc00c8c86b3f8e13f62f8dbb28b
Dumisani <ndumisani@gmail.com>
2026-01-19 10:11:47 +0200

    X variable

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Say Hi function
 function sayHi(name) {
-  let Dum = "Dumisani";
-  return `Hello ${name}`
+  let x = 42; // Unused variable
+  return `Hello ${name}, the x value is ${x}`; // Fixed typo in 'value'
 }
 
 module.exports = sayHi
