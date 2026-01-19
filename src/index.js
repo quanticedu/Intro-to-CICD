df1da3d22b1804fddf6ef6d2b2af47cdf7425a27
Dumisani <ndumisani@gmail.com>
2026-01-19 10:33:26 +0200

    corrected the test message

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // Say Hi function
 function sayHi(name) {
-  let x = 42; // Unused variable
+  let x = 42; // Un-used variable
   return `Hello ${name}, the x value is ${x}`; // Fixed typo in 'value'
 }
 
