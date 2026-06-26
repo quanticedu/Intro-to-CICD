83f0aa7a1b07da0a01d8090caedf39293fe62a77
Zebaman <zebaman03@gmail.com>
2026-06-26 03:49:05 +0300

    <this is the main comment>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+
+// This is the main entry point of the application. It exports a function that takes a name as an argument and returns a greeting message.
 function sayHi(name) {
   return `Hello there ${name}`
 }
 
 module.exports = sayHi
+//my comment
