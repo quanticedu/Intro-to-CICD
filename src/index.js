7586dfbf007887f9a1a623ee6bbfb79d8ba5f38b
Sunil M <mukundaraj.sunil@gmail.com>
2026-07-03 16:00:03 -0400

    trivial edit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// this is a simple function that takes a name and returns a greeting message
+// this is a simple function that takes a name and returns a greeting message - trivial edit
 function sayHi(name) {
   return `Hello there ${name}`
 }
