6c41115975825e35df57f53b0c34e5fc27e67db5
Kunle <kaysmailstore@gmail.com>
2026-08-06 21:13:55 +0100

    Addition of comment to program

--- src/index.js
+++ src/index.js
@@ -3,3 +3,26 @@ function sayHi(name) {
 }
 
 module.exports = sayHi
+
+// addComment.js
+const fs = require('fs');
+
+// Get file path and comment from command line
+const [,, filePath, comment] = process.argv;
+
+if (!filePath || !comment) {
+  console.log("Usage: node addComment.js <file.js> \"Your comment\"");
+  process.exit(1);
+}
+
+// Read the file
+const content = fs.readFileSync(filePath, 'utf8');
+
+// Prepend the comment
+const newContent = `// ${comment}\n${content}`;
+
+// Write back to the file
+fs.writeFileSync(filePath, newContent, 'utf8');
+
+console.log(`Comment added to ${filePath}`);
+
